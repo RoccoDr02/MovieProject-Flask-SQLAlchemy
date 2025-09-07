41150553e59ce793ffd4c238761fb3a8e07e3677
RoccoDr02 <roccodrafz@icloud.com>
2025-09-07 10:21:14 +0200

    added user and movie class

--- models.py
+++ models.py
@@ -4,11 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
-class Movie(db.Model):
-  # Define all the Movie properties
+    movies = db.relationship('Movie', backref=db.backref('user', lazy=True))
 
-  # Link Movie to User
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+class Movie(db.Model):
+    __tablename__ = "movies"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    release_date = db.Column(db.Date, nullable=True)
+    genre = db.Column(db.String(50), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Movie {self.title}>"
